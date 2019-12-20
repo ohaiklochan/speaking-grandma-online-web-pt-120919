589fab303cab99540d4707f7c675afefdf604edf
Chloe Foronda <caforonda103@yahoo.com>
2019-12-20 01:57:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,6 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
+you = speak_to_grandma
 
 if speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
