f965bfa850703ee0dda901dfadb490418464b4f3
Chloe Foronda <caforonda103@yahoo.com>
2019-12-20 01:48:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-gradma = "Hard of hearing"
+gradma == "Hard of hearing"
 
 if grandma == "SPEAK"
   puts "NO, NOT SINCE 1938!"
